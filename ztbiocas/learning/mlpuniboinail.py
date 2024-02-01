b7e45addf05ac77d9151b705f4d6f1002b8c901f
Marcello Zanghieri <marcello.zanghieri2@unibo.it>
2024-02-01 13:23:27 +0000

    sadf

--- ztbiocas/learning/mlpuniboinail.py
+++ ztbiocas/learning/mlpuniboinail.py
@@ -1,33 +1,18 @@
-"""
-    Author(s):
-    Marcello Zanghieri <marcello.zanghieri2@unibo.it>
-    
-    Copyright (C) 2023 University of Bologna and ETH Zurich
-    
-    Licensed under the GNU Lesser General Public License (LGPL), Version 2.1
-    (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-    
-        https://www.gnu.org/licenses/lgpl-2.1.txt
-    
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-"""
-
 from __future__ import annotations
 
 import torch  # for tensors
 from torch import nn
 import torchinfo
 
-from online_semg_posture_adaptation import dataset as ds
-
 
 """
+
+###############################################################################
+--> FOR THE EXTENSION, THIS ONLY HOLDS FOR hidden=8!!!
+--> decide heurostic for the other dataset(s): 8, +4, x2, **1.5, ...
+###############################################################################
+
+
 This module implements in PyTorch the 2-layer Multi-Layer Perceptron (MLP)
 termed Neural Network (NN) and used in the paper:
 
@@ -46,18 +31,15 @@ Differences:
 """
 
 
-HIDDEN_UNITS = 8
-
-
 class MLPUniboINAIL(nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_input, num_hidden, num_output):
         super(MLPUniboINAIL, self).__init__()
 
-        self.fc0 = nn.Linear(ds.NUM_CHANNELS, HIDDEN_UNITS, bias=False)
-        self.fc0_bn = nn.BatchNorm1d(8)
+        self.fc0 = nn.Linear(num_input, num_hidden, bias=False)
+        self.fc0_bn = nn.BatchNorm1d(num_hidden)
         self.fc0_relu = nn.ReLU()
-        self.fc1 = nn.Linear(8, ds.NUM_CLASSES, bias=False)
+        self.fc1 = nn.Linear(num_hidden, num_output, bias=False)
 
     def forward(self, x: torch.tensor) -> torch.tensor:
         x = self.fc0(x)
@@ -69,12 +51,13 @@ class MLPUniboINAIL(nn.Module):
 
 def summarize(
     model: nn.Module,
+    num_input: int,
     verbose: 0 | 1 | 2 = 0,
 ) -> torchinfo.ModelStatistics:
 
     # set all parameters for torchsummary
 
-    input_size = (ds.NUM_CHANNELS,)
+    input_size = (num_input,)
     batch_dim = 0  # index of the batch dimension
     col_names = [
         'input_size',
--- ztbiocas/protocol.py
+++ ztbiocas/protocol.py
@@ -1,23 +1,3 @@
-"""
-    Author(s):
-    Marcello Zanghieri <marcello.zanghieri2@unibo.it>
-    
-    Copyright (C) 2023 University of Bologna and ETH Zurich
-    
-    Licensed under the GNU Lesser General Public License (LGPL), Version 2.1
-    (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-    
-        https://www.gnu.org/licenses/lgpl-2.1.txt
-    
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-"""
-
 from __future__ import annotations
 
 import numpy as np
@@ -25,23 +5,24 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import torch.nn  # just for nn.Module
 
-from online_semg_posture_adaptation import dataset as ds
-from online_semg_posture_adaptation.online import pca as opca
-from online_semg_posture_adaptation.learning import learning as learn
-from online_semg_posture_adaptation.learning import goodness as good
+from online import pca as opca
+from learning.learning import do_inference
 
 
 def calibration_experiment(
     xcalib: np.ndarray[np.float32],
-    ycalib: np.ndarray[np.uint8],
+    ycalib: np.ndarray,  # dtype of classification or regression
     xvalid: np.ndarray[np.float32],
-    yvalid: np.ndarray[np.uint8],
+    yvalid: np.ndarray,  # dtype of classification or regression
     adapt_flag: bool,
-    stdscaler_train: StandardScaler | None,
+    beta: float,
+    stdscaler_train: StandardScaler,
     pca_train: PCA | None,
     model: torch.nn.Module,
     output_scale: float = 1.0,
-) -> tuple[dict, dict]:
+) -> tuple[np.ndarray, np.ndarray]:  # dtype of classification or regression
+
+    num_channels = xcalib.shape[0]
 
     xcalib_std = stdscaler_train.transform(xcalib.T).T
     xvalid_std = stdscaler_train.transform(xvalid.T).T
@@ -49,16 +30,16 @@ def calibration_experiment(
 
     if adapt_flag:
 
-        # --------------------------------------------------------------------#
-        # --------------------------------------------------------------------#
-        # refit the PCA online
+        # online PCA
 
         W_init = opca.initialize_online_pca(
-            ds.NUM_CHANNELS, opca.InitMode.CUSTOM, pca_train.components_.T)
+            num_channels, opca.InitMode.CUSTOM, pca_train.components_.T)
         num_samples_calib = xcalib_std.shape[1]
-        beta = 0.01
         ids_samples = np.arange(num_samples_calib)
+        
+        # scheduling
         gamma_scheduled = 1.0 / (1.0 + ids_samples / beta)
+        
         W_sequence, mean_calib, scale_calib = opca.oja_sga_session(
             xcalib_std, W_init, gamma_scheduled)
         W_calib = W_sequence[-1]
@@ -70,8 +51,8 @@ def calibration_experiment(
 
         xcalib_std = (xcalib_std - mean_calib) / scale_calib
         xvalid_std = (xvalid_std - mean_calib) / scale_calib
-        # ------------------------------------------------------------------- #
-        # ------------------------------------------------------------------- #
+        
+        # apply PCA
 
         xcalib_pc = W_calib.T @ xcalib_std
         xvalid_pc = W_calib.T @ xvalid_std
@@ -83,20 +64,17 @@ def calibration_experiment(
 
     del xcalib_std, xvalid_std
 
-    # MLP inference
-    yout_calib = learn.do_inference(xcalib_pc, model, output_scale)
-    yout_valid = learn.do_inference(xvalid_pc, model, output_scale)
+    # inference
+    yout_calib = do_inference(xcalib_pc, model, output_scale)
+    yout_valid = do_inference(xvalid_pc, model, output_scale)
     del xcalib_pc, xvalid_pc
 
-    metrics_calib = good.compute_classification_metrics(ycalib, yout_calib)
-    metrics_valid = good.compute_classification_metrics(yvalid, yout_valid)
-
-    return metrics_calib, metrics_valid
+    return yout_calib, yout_valid
 
 
 def main() -> None:
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
