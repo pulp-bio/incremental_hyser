1cc117a383b9972c69c295917c74fc231831b8b6
Marcello Zanghieri <marcello.zanghieri2@unibo.it>
2024-02-05 18:17:59 +0000

    asf

--- incremental_hyser/learning/mlpuniboinail.py
+++ incremental_hyser/learning/mlpuniboinail.py
@@ -7,45 +7,94 @@ import torchinfo
 
 """
 
-###############################################################################
---> FOR THE EXTENSION, THIS ONLY HOLDS FOR hidden=8!!!
---> decide heurostic for the other dataset(s): 8, +4, x2, **1.5, ...
-###############################################################################
+This module implements in PyTorch the ... TEMPONET...
 
-
-This module implements in PyTorch the 2-layer Multi-Layer Perceptron (MLP)
-termed Neural Network (NN) and used in the paper:
-
-B. Milosevic, E. Farella, S. Benatti,
-Exploring Arm Posture and Temporal Variability in Myoelectric Hand Gesture
-Recognition
-https://doi.org/10.1109/BIOROB.2018.8487838
+...citation...
 
 Differences:
-- ReLU non-linear activation function is used instead of sigmoid;
-- Batch-Normalization (BN) is added;
-- the structure is adapted to be compliant with the DNN quantization tool
-  quantlib (https://github.com/pulp-platform/quantlib): no biases, no final
-  softmax inside the model, so that the model ends with a biasless linear.
+- ...
+- ...
 
 """
 
 
-class MLPUniboINAIL(nn.Module):
+class TEMPONet(nn.Module):
+    
+    def __init__(self,
+            num_ch_in: int,
+            num_ch_out: int,
+            pdrop_fc: int=0.5,
+        ):
+        super().__init__()
+        
+        self.num_ch_in = num_ch_in
+        self.num_ch_out = num_ch_out
+        
+        self.b0_tcn0      = nn.Conv1d(self.Cx, 16, 3, dilation=2, padding=2, bias=False)
+        self.b0_tcn0_BN   = nn.BatchNorm1d(16)
+        self.b0_tcn0_ReLU = nn.ReLU()
+        self.b0_tcn1      = nn.Conv1d(16, 16, 3, dilation=2, padding=2, bias=False)
+        self.b0_tcn1_BN   = nn.BatchNorm1d(16)
+        self.b0_tcn1_ReLU = nn.ReLU()
+        self.b0_conv      = nn.Conv1d(16, 16, 5, stride=1, padding=2, bias=False)
+        self.b0_conv_pool = torch.nn.AvgPool1d(2, stride=2, padding=0)
+        self.b0_conv_BN   = nn.BatchNorm1d(16)
+        self.b0_conv_ReLU = nn.ReLU()
 
-    def __init__(self, num_input, num_hidden, num_output):
-        super(MLPUniboINAIL, self).__init__()
+        self.b1_tcn0      = nn.Conv1d(16, 32, 3, dilation=4, padding=4, bias=False)
+        self.b1_tcn0_BN   = nn.BatchNorm1d(32)
+        self.b1_tcn0_ReLU = nn.ReLU()
+        self.b1_tcn1      = nn.Conv1d(32, 32, 3, dilation=4, padding=4, bias=False)
+        self.b1_tcn1_BN   = nn.BatchNorm1d(32)
+        self.b1_tcn1_ReLU = nn.ReLU()
+        self.b1_conv      = nn.Conv1d(32, 32, 5, stride=2, padding=2, bias=False)
+        self.b1_conv_pool = torch.nn.AvgPool1d(2, stride=2, padding=0)
+        self.b1_conv_BN   = nn.BatchNorm1d(32)
+        self.b1_conv_ReLU = nn.ReLU()
 
-        self.fc0 = nn.Linear(num_input, num_hidden, bias=False)
-        self.fc0_bn = nn.BatchNorm1d(num_hidden)
-        self.fc0_relu = nn.ReLU()
-        self.fc1 = nn.Linear(num_hidden, num_output, bias=False)
+        self.b2_tcn0      = nn.Conv1d(32, 64, 3, dilation=8, padding=8, bias=False)
+        self.b2_tcn0_BN   = nn.BatchNorm1d(64)
+        self.b2_tcn0_ReLU = nn.ReLU()
+        self.b2_tcn1      = nn.Conv1d(64, 64, 3, dilation=8, padding=8, bias=False)
+        self.b2_tcn1_BN   = nn.BatchNorm1d(64)
+        self.b2_tcn1_ReLU = nn.ReLU()
+        self.b2_conv      = nn.Conv1d(64, 64, 5, stride=4, padding=2, bias=False)
+        self.b2_conv_pool = torch.nn.AvgPool1d(2, stride=2, padding=0)
+        self.b2_conv_BN   = nn.BatchNorm1d(64)
+        self.b2_conv_ReLU = nn.ReLU()
+        
+        self.fc0         = nn.Linear(64 * 4, 64, bias=False)
+        self.fc0_bn      = nn.BatchNorm1d(64)
+        self.fc0_relu    = nn.ReLU()
+        self.fc0_drop = nn.Dropout(0.5)
 
-    def forward(self, x: torch.tensor) -> torch.tensor:
-        x = self.fc0(x)
-        x = self.fc0_bn(x)
-        x = self.fc0_relu(x)
-        y = self.fc1(x)
+        self.fc0         = nn.Linear(64 * 4, 64, bias=False)
+        self.fc0_bn      = nn.BatchNorm1d(64)
+        self.fc0_relu    = nn.ReLU()
+        self.fc0_drop = nn.Dropout(0.5)
+        
+     
+        self.fc2 = nn.Linear(32, self.num_ch_out, bias=False)
+        
+    def forward(self, x):
+
+        x = self.b0_tcn0_ReLU(self.b0_tcn0_BN(                  self.b0_tcn0(x) ))
+        x = self.b0_tcn1_ReLU(self.b0_tcn1_BN(                  self.b0_tcn1(x) ))
+        x = self.b0_conv_ReLU(self.b0_conv_BN(self.b0_conv_pool(self.b0_conv(x))))
+
+        x = self.b1_tcn0_ReLU(self.b1_tcn0_BN(                  self.b1_tcn0(x) ))
+        x = self.b1_tcn1_ReLU(self.b1_tcn1_BN(                  self.b1_tcn1(x) ))
+        x = self.b1_conv_ReLU(self.b1_conv_BN(self.b1_conv_pool(self.b1_conv(x))))
+        
+        x = self.b2_tcn0_ReLU(self.b2_tcn0_BN(                  self.b2_tcn0(x) ))
+        x = self.b2_tcn1_ReLU(self.b2_tcn1_BN(                  self.b2_tcn1(x) ))
+        x = self.b2_conv_ReLU(self.b2_conv_BN(self.b2_conv_pool(self.b2_conv(x))))
+        
+        x = x.flatten(1)
+        x = self.FC0_dropout(self.FC0_ReLU(self.FC0_BN(self.FC0(x))))
+        x = self.FC1_dropout(self.FC1_ReLU(self.FC1_BN(self.FC1(x))))
+        y = self.GwayFC(x)
+        
         return y
 
 
@@ -97,10 +146,10 @@ def main() -> None:
     # Display the summary of the MLP
 
     verbose = 1
-    mlp_unibo_inail = MLPUniboINAIL()
-    mlp_unibo_inail.eval()
-    mlp_model_stats = summarize(mlp_unibo_inail, verbose=verbose)
+    temponet = TEMPONet()
+    temponet.eval()
+    mlp_model_stats = summarize(temponet, verbose=verbose)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
