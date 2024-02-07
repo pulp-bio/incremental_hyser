fixed force scaling and interp

incremental_hyser/hyser/hyser.py
@@ -330,7 +330,14 @@ def load_hdsemg_and_force(
         idx_trial=idx_trial,
     )
 
-    return hdsemg_v, force_v
+    # interpolate the force
+    num_hi = hdsemg_v.shape[1]
+    force_v_interp = np.zeros((NUM_CHANNELS_FORCE, num_hi), dtype=np.float32)
+    for idx_hi in range(num_hi):
+        idx_lo = idx_hi * FS_FORCE // FS_HDSEMG
+        force_v_interp[:, idx_lo]
+
+    return hdsemg_v, force_v_interp
 
 
 def concatenate_segments(

incremental_hyser/learning/learning.py
@@ -91,7 +91,7 @@ def _collate_xy_pairs(
 
 
 @enum.unique
-class _LoaderMode(enum.Enum):
+class LoaderMode(enum.Enum):
     TRAINING_RANDOMIZED = 'TRAINING_RANDOMIZED'
     TRAINING_SEQUENTIAL = 'TRAINING_SEQUENTIAL'
     INFERENCE = 'INFERENCE'
@@ -99,27 +99,27 @@ class _LoaderMode(enum.Enum):
 
 def _dataset2dataloader(
     dataset: _EMGPytorchDataset,
-    loadermode: _LoaderMode,
+    loadermode: LoaderMode,
     minibatch_size: int | None,
 ) -> torch.utils.data.DataLoader:
 
-    assert isinstance(loadermode, _LoaderMode)
-    if loadermode is _LoaderMode.INFERENCE:
+    assert isinstance(loadermode, LoaderMode)
+    if loadermode is LoaderMode.INFERENCE:
         assert minibatch_size is None
     else:
         assert minibatch_size is not None
 
-    if loadermode is _LoaderMode.TRAINING_RANDOMIZED:
+    if loadermode is LoaderMode.TRAINING_RANDOMIZED:
         batch_size = minibatch_size
         drop_last = True
         shuffle = True
         sampler = None
-    if loadermode is _LoaderMode.TRAINING_SEQUENTIAL:
+    if loadermode is LoaderMode.TRAINING_SEQUENTIAL:
         batch_size = minibatch_size
         drop_last = True
         shuffle = False
         sampler = torch.utils.data.SequentialSampler(dataset)
-    elif loadermode is _LoaderMode.INFERENCE:
+    elif loadermode is LoaderMode.INFERENCE:
         batch_size = MINIBATCH_SIZE_INFER
         drop_last = False
         shuffle = False
@@ -148,7 +148,7 @@ def do_inference(
 ) -> tuple:
 
     dataset = _EMGPytorchDataset(x)
-    dataloader = _dataset2dataloader(dataset, mode=_LoaderMode.INFERENCE)
+    dataloader = _dataset2dataloader(dataset, mode=LoaderMode.INFERENCE)
 
     model.eval()
     model.to(DEVICE)
@@ -181,9 +181,9 @@ def do_training(
     yvalid: np.ndarray[np.uint8 | np.float32] | None,
     model: torch.nn.Module,
     loadermode_train: \
-        [_LoaderMode.TRAINING_RANDOMIZED, _LoaderMode.TRAINING_SEQUENTIAL],
+        [LoaderMode.TRAINING_RANDOMIZED, LoaderMode.TRAINING_SEQUENTIAL],
     minibatch_train: int,
-    minibatch_valid: int,
+    # minibatch_valid: int,
     num_epochs: int,
     criterion: torch.nn.Module | None = None,  # None as default (ugly)
     optimizer: torch.optim.Optimizer | None = None,  # None as default (ugly)
@@ -192,11 +192,11 @@ def do_training(
 
     assert (xvalid is None) == (yvalid is None)
     assert loadermode_train in \
-        [_LoaderMode.TRAINING_RANDOMIZED, _LoaderMode.TRAINING_SEQUENTIAL]
+        [LoaderMode.TRAINING_RANDOMIZED, LoaderMode.TRAINING_SEQUENTIAL]
     
     dataset_train = _EMGPytorchDataset(xtrain, ytrain)
     dataloader_train = _dataset2dataloader(
-        dataset_train, loadermode=loadermode_train)
+        dataset_train, loadermode_train, minibatch_train)
 
     model.to(DEVICE)
     model.train()

incremental_hyser/learning/models.py
@@ -103,7 +103,7 @@ class TinierNet8(nn.Module):
     def __init__(self,
             num_ch_in: int,
             num_ch_out: int,
-            pdrop_fc: int=0.0,
+            pdrop_fc: float=0.0,
         ):
         super().__init__()
         
@@ -111,37 +111,45 @@ class TinierNet8(nn.Module):
         self.num_ch_out = num_ch_out
         self.pdrop_fc = pdrop_fc
         
-        self.b0_conv = nn.Conv1d(self.num_ch_in, 32, 3, padding='valid', stride=2, bias=False)
+        self.b0_conv = nn.Conv1d(self.num_ch_in, 32, 3, padding='same', bias=False)
         self.b0_bn = nn.BatchNorm1d(32)
         self.b0_relu = nn.ReLU()
+        self.b0_pool = nn.AvgPool1d(2, stride=2)
 
-        self.b1_conv = nn.Conv1d(32, 32, 3, padding='valid', stride=2, bias=False)
+        self.b1_conv = nn.Conv1d(32, 32, 3, padding='same', bias=False)
         self.b1_bn = nn.BatchNorm1d(32)
         self.b1_relu = nn.ReLU()
+        self.b1_pool = nn.AvgPool1d(2, stride=2)
 
-        self.b2_conv = nn.Conv1d(32, 32, 3, padding='valid', stride=2, bias=False)
+        self.b2_conv = nn.Conv1d(32, 32, 3, padding='same', bias=False)
         self.b2_bn = nn.BatchNorm1d(32)
         self.b2_relu = nn.ReLU()
+        self.b2_pool = nn.AvgPool1d(2, stride=2)
 
-        self.b3_conv = nn.Conv1d(32, 32, 3, padding='valid', stride=2, bias=False)
+        self.b3_conv = nn.Conv1d(32, 32, 3, padding='same', bias=False)
         self.b3_bn = nn.BatchNorm1d(32)
         self.b3_relu = nn.ReLU()
+        self.b3_pool = nn.AvgPool1d(2, stride=2)
 
-        self.b4_conv = nn.Conv1d(32, 64, 3, padding='valid', stride=2, bias=False)
+        self.b4_conv = nn.Conv1d(32, 64, 3, padding='same', bias=False)
         self.b4_bn = nn.BatchNorm1d(64)
         self.b4_relu = nn.ReLU()
+        self.b4_pool = nn.AvgPool1d(2, stride=2)
 
-        self.b5_conv = nn.Conv1d(64, 64, 3, padding='valid', stride=2, bias=False)
+        self.b5_conv = nn.Conv1d(64, 64, 3, padding='same', bias=False)
         self.b5_bn = nn.BatchNorm1d(64)
         self.b5_relu = nn.ReLU()
+        self.b5_pool = nn.AvgPool1d(2, stride=2)
 
-        self.b6_conv = nn.Conv1d(64, 64, 3, padding='valid', stride=2, bias=False)
+        self.b6_conv = nn.Conv1d(64, 64, 3, padding='same', bias=False)
         self.b6_bn = nn.BatchNorm1d(64)
         self.b6_relu = nn.ReLU()
+        self.b6_pool = nn.AvgPool1d(2, stride=2)
 
-        #self.b7_conv = nn.Conv1d(64, 64, 3, padding='valid', stride=2, bias=False)
-        #self.b7_bn = nn.BatchNorm1d(1)
-        #self.b7_relu = nn.ReLU()
+        self.b7_conv = nn.Conv1d(64, 64, 3, padding='same', bias=False)
+        self.b7_bn = nn.BatchNorm1d(64)
+        self.b7_relu = nn.ReLU()
+        self.b7_pool = nn.AvgPool1d(2, stride=2)
         
         self.fc0 = nn.Linear(64, 64, bias=False)
         self.fc0_bn = nn.BatchNorm1d(64)
@@ -157,14 +165,14 @@ class TinierNet8(nn.Module):
         
     def forward(self, x):
 
-        x = self.b0_relu(self.b0_bn(self.b0_conv(x)))
-        x = self.b1_relu(self.b1_bn(self.b1_conv(x)))
-        x = self.b2_relu(self.b2_bn(self.b2_conv(x)))
-        x = self.b3_relu(self.b3_bn(self.b3_conv(x)))
-        x = self.b4_relu(self.b4_bn(self.b4_conv(x)))
-        x = self.b5_relu(self.b5_bn(self.b5_conv(x)))
-        x = self.b6_relu(self.b6_bn(self.b6_conv(x)))
-        #x = self.b7_relu(self.b7_bn(self.b7_conv(x)))
+        x = self.b0_pool(self.b0_relu(self.b0_bn(self.b0_conv(x))))
+        x = self.b1_pool(self.b1_relu(self.b1_bn(self.b1_conv(x))))
+        x = self.b2_pool(self.b2_relu(self.b2_bn(self.b2_conv(x))))
+        x = self.b3_pool(self.b3_relu(self.b3_bn(self.b3_conv(x))))
+        x = self.b4_pool(self.b4_relu(self.b4_bn(self.b4_conv(x))))
+        x = self.b5_pool(self.b5_relu(self.b5_bn(self.b5_conv(x))))
+        x = self.b6_pool(self.b6_relu(self.b6_bn(self.b6_conv(x))))
+        x = self.b7_pool(self.b7_relu(self.b7_bn(self.b7_conv(x))))
 
         x = x.flatten(1)
         x = self.fc0_drop(self.fc0_relu(self.fc0_bn(self.fc0(x))))
@@ -220,10 +228,15 @@ def summarize(
 
 def main() -> None:
 
-    verbose = 2
-    tiniernet8 = TinierNet8(64, 5)
+    num_ch_in = 64
+    num_samples_in = 256
+    num_ch_out = 5
+
+    tiniernet8 = TinierNet8(num_ch_in, num_ch_out)
     tiniernet8.eval()
-    summarize(tiniernet8, 64, 256, verbose=verbose)
+
+    verbose = 2
+    summarize(tiniernet8, num_ch_in, num_samples_in, verbose=verbose)
 
 
 if __name__ == "__main__":